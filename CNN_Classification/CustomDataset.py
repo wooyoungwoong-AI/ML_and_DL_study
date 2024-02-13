146a6ff985af13a0dd9ad8515ceb70f8aaed6bb4
youngwoong woo <uyeongung06@gmail.com>
2024-02-13 18:17:44 +0900

    Add files via upload

---
+++ CNN_Classification/CustomDataset.py
@@ -0,0 +1,33 @@
+from torch.utils.data import Dataset
+
+import os
+from PIL import Image
+
+class CustomDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.classes = sorted(os.listdir(root_dir))
+        self.class_to_idx = {cls : idx for cls, idx in enumerate(self.classes)}
+        self.images = self.make_dataset()
+    
+    def make_dataset(self):
+        images = []
+        for class_name in self.classes:
+            class_dir = os.path.join(self.root_dir, class_name)
+            if not os.path.isdir(class_dir):
+                continue
+            for img_name in os.listdir(class_dir):
+                img_path = os.path.join(class_dir, img_name)
+                images.append((img_path, self.class_to_idx[class_name]))
+        return images
+    
+    def __len__(self):
+        return len(self.images)
+    
+    def __getitem__(self, idx):
+        img_path, label = self.images[idx]
+        image = Image.open(img_path).convert('RGB')
+        if self.transform:
+            image = self.transform(image)
+        return image, label
